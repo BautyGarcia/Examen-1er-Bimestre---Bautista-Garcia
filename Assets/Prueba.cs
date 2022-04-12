cb48a610d3abdbfa4a745cb58c68df23337e4558
BautyGarcia <bautistaroccogarcia@gmail.com>
2022-04-12 15:27:57 -0300

    PruebaFinished
    Nice

---
+++ Assets/Prueba.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    public float CantTaxis;
+    public float CantDias;
+    public float CantLluviosos;
+    float costoTotal;
+    float diasMSG;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CantLluviosos < 0 || CantLluviosos > CantDias || CantDias < 5){
+            Debug.Log("No es posible esa cantidad de dias lluviosos ó Los dias no pueden ser menos que 5");
+            return;
+        }
+        diasMSG = CantDias;
+        CantDias -= CantLluviosos;
+
+        CantLluviosos = CantLluviosos * 110;
+        CantLluviosos = CantLluviosos / 15;
+        CantLluviosos = CantLluviosos * 130;
+
+        CantDias = CantDias * 90;
+        CantDias = CantDias / 15;
+        CantDias = CantDias * 130;
+
+        costoTotal += CantDias;
+        costoTotal += CantLluviosos;
+
+        costoTotal = costoTotal * CantTaxis;
+
+        Debug.Log("Una flota de "+CantTaxis+" unidades trabajando durante "+diasMSG+" días implicará un gasto de "+costoTotal+" pesos en concepto de combustible");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    
+   
+}
